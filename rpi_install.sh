8725542c5944f19dfee60868c1007206a67bd4b0
jakka351 <57064943+jakka351@users.noreply.github.com>
2020-10-19 18:19:01 +1100

    Create rpi_install.sh
    install script raspbian lite start

---
+++ rpi_install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#rpi_install.sh
+#this script will install all functions to a fresh install of raspbian lite or raspbian/Raspberry Pi OS
+
+sudo apt update && sudo apt dist-upgrade
+
+sudo apt install bluetooth bluez blueman minicom python python3 wiring-pi
+
+#set up bluetooth serial port profile
+
+#set hostname
+
+#set gpio defaults
+
+#set gpsd socket
+
+#set wifi off
+
+
