d94921da327ef4e824bb99dc5805b09810aaecf0
jakka351 <57064943+jakka351@users.noreply.github.com>
2020-11-08 00:45:09 +1100

    BLE GPIO Control

--- ble/README.md
+++ ble/README.md
@@ -1,3 +1,150 @@
-#Bluetooth Low Energy Server
+# boxee
+**Work in Progress**
 
- ble
+Boxee is a Bluetooth Low Energy automation protoype for the Raspberyy PI.
+It relies on Dbus and Bluez to expose GPIO control over the BLE, so that one can control GPIOs over the phone.
+The testing application on IOS is *LightBlue*
+
+## Current Status
+
+Creates a Bluetooth LE advertisement and publishes one service, which enables to set GPIO 17 and 18 to HIGH and LOW on a Raspberry PI, by writing a 2 byte value array:
+* 0x00 0x00 => PIN 17 and 18 is LOW
+* 0x00 0xFF => PIN 17 is LOW, PIN 18 is HIGH
+* 0xFF 0xFF => PIN 17 and 18 are HIGH
+
+### Other features
+* The logging is made in syslog, some text is still printed on the standard ouput
+* Tested with RPI2
+
+## Current Limitations
+* only GPIO 17 and 18 are initialized and controllable, however this limitation can be easily overcome by adding more channels in the BoxeeServer constructor (out_chs = [17, 18, xx, xx])
+* sometimes the LE Advertisement is restarted only 15 seconds after the connection was removed (bluez problem?)
+* there's no notification or read support, however the complete infrastructure implementation is finished
+* the GATT server and the LE adverstisement are marked to be experimental features in the Bluez stack
+* there's no security whatsoever (anybody can send low or high requests to the GPIO interfaces)
+
+## Current Issues
+* the ERROR level is not logged in the syslog for some reason
+* the LE Advertisement is not removed for some reason, once the server is shut down (bluez problem?)
+* there are still some methods printing to the standard output some debug data
+* the company data within the advertisement packages is not yet configurable from the Boxee Server
+
+# Setup and dependencies
+
+## Dependencies
+* The Boxee server is based on the most recent bluez 5.34
+* Python 2.x
+* Systemd is required for the bluez initialization (*soft* bluez requirement, it can be overcome by --disable-systemd configuration directive on bluez)
+
+# Debugging and development
+
+## Dbus Monitoring
+
+The best done via dbus-monitor. First one needs to configure the Dbus in a way that can have an eye on every command/signal on the bus
+
+cat /etc/dbus-1/system-local.conf
+```bash
+<!DOCTYPE busconfig PUBLIC
+"-//freedesktop//DTD D-Bus Bus Configuration 1.0//EN"
+"http://www.freedesktop.org/standards/dbus/1.0/busconfig.dtd">
+<busconfig>
+    <policy user="root">
+        <allow eavesdrop="true"/>
+        <allow eavesdrop="true" send_destination="*"/>
+    </policy>
+</busconfig>
+```
+Then: dbus-monitor --system --monitor
+
+## Installation of Bluez
+```bash
+sudo apt-get install tmux libusb-dev libdbus-1-dev libglib2.0-dev libudev-dev libreadline-dev libical0 libical-dev rfkill libnss-myhostname
+sudo mkdir bluez
+cd bluez
+
+sudo wget https://www.kernel.org/pub/linux/bluetooth/bluez-5.36.tar.gz
+sudo tar xvf bluez-5.36.tar
+cd bluez-5.36/
+sudo rfkill unblock all
+
+LDFLAGS=-lrt
+
+sudo ./configure --enable-experimental --enable-maintainer-mode --enable-library --sysconfdir=/etc --prefix=/usr --mandir=/usr/share/man --localstatedir=/var --enable-tools
+
+sudo make
+sudo make install
+```
+
+Then you need to edit /lib/systemd/system/bluetooth.service and add **-nE** to the bluetoothd. This will enable to start the experimental features of the bluetooth daemon.
+
+Create a link in: /etc/systemd/system
+```
+dbus-org.bluez.service -> /lib/systemd/system/bluetooth.service
+```
+Than
+```bash
+systemctl --system daemon-reload
+systemctl restart bluetooth.service
+```
+
+## Monitoring bluez activities
+
+**For low level happenings**
+```bash
+btmon
+```
+
+## bluetoothctl
+ * list known devices: 'devices'
+ * list all bluetooth controllers: 'list'
+ * show bluetooth controller details: 'show'
+ 
+
+## Hints and tricks
+You can connect to BLE enbaled devices by using:
+* if the bluetooth address is not public
+..* hcitool lecc --random <MAC>
+* if the bluetooth address is public
+..* hcitool lecc <MAC>
+
+Same goes with the gatttool:
+* gatttool -t random -b <MAC> -I or
+* gatttool -b <MAC> -I
+
+One can check if the address is public or not by running *hcidump -X* and checking the address type while running *hcitool lescan*;
+
+In case one have various connection errors you might try:
+```bash
+bluez*/tools/btmgmt pairable on
+bluez*/tools/btmgmt le on
+```
+Also sometimes the connection error is due to
+
+## Other dependencines
+psutil module is used to send memory and CPU information
+```bash
+apt-get install python-psutil python3-psutil
+```
+
+## Sources
+* Good and general summary of GATT: https://learn.adafruit.com/introduction-to-bluetooth-low-energy/gatt
+* GATT Profile hierarchy (official documentation): https://developer.bluetooth.org/TechnologyOverview/Pages/GATT.aspx
+* Ibeacon definition: https://developer.mbed.org/blog/entry/BLE-Beacons-URIBeacon-AltBeacons-iBeacon/
+
+## Further reading
+* Very good general presentation: http://www.slideshare.net/yeokm1/introduction-to-bluetooth-low-energy
+
+## Running and monitoring the app
+```bash
+python /tmp/boxee/boxee.py -d
+tail -fn 300 /var/log/syslog
+```
+And if you'd like to check the status of the database: 
+```bash
+sqlite3 /tmp/boxee/boxee.db 
+sqlite> select * from locker;
+```
+
+
+
+
---
+++ ble/boxee.py
@@ -0,0 +1,331 @@
+#!/usr/bin/python
+
+import dbus, dbus.mainloop.glib, dbus.service, gobject
+from dbus.exceptions import DBusException
+from boxee.box_service import BoxService
+from boxee.exceptions import DoesNotExistException, FailedException, InvalidArgsException, InvalidValueLengthException, \
+    NotPermittedException, NotSupportedException
+import sys, os, getopt, logging, logging.handlers
+from boxee import stypes
+import boxee.core, boxee.io_service, boxee.advertisement, boxee.utils, boxee.gpio
+from boxee.gpio import GpioConnector
+from boxee.io_service import AutomationIOService
+from boxee.system_service import SystemService
+from boxee.advertisement import BoxAdvertisement
+import boxee.persistence
+import boxee.box_service
+
+mainloop = None
+logger = logging.getLogger(__name__)
+
+
+class BoxeeServer:
+    """
+     The main server controlling the IO breakouts of the raspberry PI over bluetooth low energy connections (GATT)
+    """
+    # todo: bluetoothd[1792]: Can't store info for private addressed device
+
+    def __init__(self, current_folder, log_level):
+
+        self.setup_logging(current_folder, log_level)
+        # GPIO configuration
+        out_chs = [17, 18]
+        self.box_dao = boxee.persistence.BoxDao(range(17, 19), current_folder)
+
+        self.gpio = GpioConnector(out_channels=out_chs)
+
+        dbus.mainloop.glib.DBusGMainLoop(set_as_default=True)
+
+        self.bus = dbus.SystemBus()
+
+        self.gatt_adapter = self.find_adapter_for_interface(self.bus, boxee.core.GATT_MGR_IFACE)
+        self.advertising_adapter = self.find_adapter_for_interface(self.bus, boxee.core.LE_ADVERTISING_MANAGER_IFACE)
+        if self.gatt_adapter != self.advertising_adapter:
+            err = ' the gatt adapter and the advertising adapters are not the same. Exiting application...'
+            print(err)
+            logger.error(err)
+            sys.exit(-1)
+
+        self.gatt_manager = dbus.Interface(
+            self.bus.get_object(boxee.core.BLUEZ_SERVICE_NAME, self.gatt_adapter),
+            boxee.core.GATT_MGR_IFACE)
+
+        self.advertising_manager = dbus.Interface(
+            self.bus.get_object(boxee.core.BLUEZ_SERVICE_NAME, self.gatt_adapter),
+            boxee.core.LE_ADVERTISING_MANAGER_IFACE)
+
+        self.hci0_props_manager = dbus.Interface(
+            self.bus.get_object(boxee.core.BLUEZ_SERVICE_NAME, self.gatt_adapter),
+            boxee.core.DBUS_PROP_IFACE)
+
+        # GATT service storage array
+        self.services = []
+
+        # Initialize bluetooth advertisement
+        self.advertisement = BoxAdvertisement(self.bus, '0')
+
+    def start_server(self):
+        """
+        Lifecycle method: is the first to be called in order to start the server;
+        """
+        start_msg = 'Starting boxee server'
+        print(start_msg)
+        logger.info(start_msg)
+
+        global mainloop
+        mainloop = gobject.MainLoop()
+
+        if self.hci0_props_manager.Get(boxee.core.ADAPTER_IFACE, 'Powered') == dbus.Boolean(0):
+            msg = 'Powering on the adapter [%s]' % self.hci0_props_manager.Get(boxee.core.ADAPTER_IFACE, 'Name')
+            print(msg)
+            logger.info(msg)
+            self.hci0_props_manager.Set(boxee.core.ADAPTER_IFACE, 'Powered', dbus.Boolean(1))
+
+        print('Adapter properties')
+        print(boxee.utils.describe_dbus_dict(self.hci0_props_manager.GetAll(boxee.core.ADAPTER_IFACE).iteritems()))
+
+        # print self.hci_props_manager.GetAll('org.bluez.GattManager1')
+
+        self.bus.add_signal_receiver(self.signal_receiver_callback,
+                                     signal_name=None,
+                                     dbus_interface=None,
+                                     bus_name=None,
+                                     path=None,
+                                     sender_keyword='sender',
+                                     destination_keyword='destination',
+                                     interface_keyword='interface',
+                                     member_keyword='member',
+                                     path_keyword='path')
+        # Setup services
+        self.services.append(AutomationIOService(self.bus, 0, write_callback_func=self.ble_service_write_cb))
+        self.services.append(SystemService(self.bus, 1, write_callback_func=self.ble_service_write_cb))
+        self.services.append(BoxService(self.box_dao, self.gpio, self.bus, 2))
+
+        for srv in self.services:
+            logger.info('Registering BLE service [%s]' % srv.get_path())
+            self.gatt_manager.RegisterService(srv.get_path(), {},
+                                              reply_handler=self.service_registration_cb,
+                                              error_handler=self.service_registration_err_cb)
+
+        logger.info('Registering BLE advertisement [%s]' % self.advertisement.get_path())
+        self.advertisement.add_service_uuid('2A56')
+        self.advertising_manager.RegisterAdvertisement(self.advertisement.get_path(), {},
+                                                       reply_handler=self.adv_registration_cb,
+                                                       error_handler=self.adv_registration_err_cb)
+
+        mainloop.run()
+
+    def stop_server(self):
+        """
+        Lifecycle method: the last to be called upon exit
+        """
+
+        # todo: unregister advertsiement is not working
+        exit_msg = 'Gracefully exiting boxee...'
+        print(exit_msg)
+        logger.info(exit_msg)
+
+        logger.debug('Cleanup on GPIO')
+        self.gpio.cleanup()
+
+        logger.info('Unregistering advertisement...')
+        try:
+            logger.debug('Unregistering service adverstisement with path [%s]', self.advertisement.get_path())
+            self.advertising_manager.UnregisterAdvertisement(self.advertisement.get_path())
+            self.advertisement.Release()
+        except (DBusException, DoesNotExistException, InvalidArgsException) as e:
+            logger.error('Could not cleanly unregister advertisement: %s' % str(e))
+        except BaseException as e:
+            logger.error('Uncategorized exception caught while unregistering advertisment: %s' % str(e))
+        for srv in self.services:
+            logger.info('Unregistering service: %s' % srv.get_path())
+            self.gatt_manager.UnregisterService(srv.get_path())
+        if self.box_dao:
+            self.box_dao.destroy()
+
+        logger.info('Boxee server is terminated...')
+
+    def service_registration_cb(self):
+        """
+        Callback method called by DBus, once the original call was succesfully executed
+        :return:
+        """
+        logger.debug('A GATT service got registered')
+
+    def service_registration_err_cb(self, error):
+        """
+        Callback method called by DBus, once the original method call was executed with an error
+            :param error: a DBusException
+        """
+        err_msg = 'Exiting. Failed to register service: ' + str(error)
+        print(err_msg)
+        logger.error(err_msg)
+        mainloop.quit()
+
+    @staticmethod
+    def adv_registration_cb():
+        logger.debug('Advertisement registered')
+
+    @staticmethod
+    def adv_registration_err_cb(error):
+        err_msg = 'Failed to register advertisement: ' + str(error)
+        print(err_msg)
+        logger.error(err_msg)
+
+    def ble_service_write_cb(self, signal_dictionary):
+        """
+        Callback method called whenever a bluetooth low energy GATT Characteristic write method is called
+        :param signal_dictionary: example: {'AutIODigitalChrc': dbus.Array([dbus.Byte(0), dbus.Byte(255)], signature=dbus.Signature('y'))}
+        :return:
+        """
+        logger.debug('BLE service write callback with signal dictionary: %s',
+                     signal_dictionary if signal_dictionary is not None else 'Undefined')
+        try:
+            if 'AutIODigitalChrc' in signal_dictionary:
+                value_array = signal_dictionary['AutIODigitalChrc']
+                if isinstance(value_array, dbus.Array):
+                    self.gpio.handle_out_channel_control_array(value_array)
+        except BaseException as e:
+            print('Unexpected error: ', sys.exc_info()[0], str(e))
+            logger.error('Error while handling bluetooth low energy callback')
+
+    @staticmethod
+    def signal_receiver_callback(*args, **kwargs):
+        """
+        Callback method registered for signals to be received on DBus
+        :param args:
+        :param kwargs:
+        :return:
+        """
+
+        # Dictionary of arguments.
+        # Example:
+        #       member == PropertiesChanged
+        #       path == /org/bluez/boxee/service0/char0
+        #       destination == None
+        #       interface == org.freedesktop.DBus.Properties
+        #       sender == :1.8
+        if logger.isEnabledFor(logging.DEBUG):
+            # args_string = ''
+            kwarg_string = ''
+            # if args is not None:
+            if kwargs is not None:
+                for key, value in kwargs.iteritems():
+                    kwarg_string += '{[%s] == [%s]}' % (key, value)
+            # args_string = ''.join(args)
+            logger.debug('Signal received: args: [%s] | Dictionary args: %s' % (args, kwarg_string))
+
+        signal = {stypes.KEY_SIG_TYPE: stypes.SIG_TYPE_UNHANDLED}
+
+        if len(args) > 0 and args[0].startswith('org.bluez.Device1'):
+            signal[stypes.KEY_SIG_TYPE] = stypes.SIG_TYPE_BLUE_DEVICE
+            if isinstance(args[1], dbus.Dictionary):
+                signal[stypes.KEY_SIG_VALUE_TYPE] = 'dictionary'
+                signal[stypes.KEY_SIG_VALUE] = args[1]
+
+        logger.debug("Signal argument list:")
+        # example value: signal_type == unhandled
+        for key, value in signal.iteritems():
+            logger.debug("\t %s == %s" % (key, value))
+
+            # Signal types:
+            # Signal argument list:
+            #   signal_type == blue_device
+            #   value_type == dictionary
+            #   value == dbus.Dictionary({dbus.String(u'Connected'): dbus.Boolean(True, variant_level=1)}, signature=dbus.Signature('sv'))
+        # -----------
+
+        # Signal processing
+        if signal[stypes.KEY_SIG_TYPE] is stypes.SIG_TYPE_BLUE_DEVICE and stypes.KEY_SIG_VALUE in signal:
+            if stypes.TEST_CONNECTED in signal[stypes.KEY_SIG_VALUE]:
+                if not signal[stypes.KEY_SIG_VALUE][stypes.TEST_CONNECTED]:
+                    logger.info('Need to reconnect')
+                else:
+                    logger.info('no need to reconnect')
+
+    @staticmethod
+    def find_adapter_for_interface(bus, iface_name):
+        """
+        Returns the first bluetooth adapter which has a org.bluez.GattManager1 interface on it
+            :param bus: the dbus handler instance
+            :return: the bluetooth adapter
+        """
+        remote_om = dbus.Interface(bus.get_object(boxee.core.BLUEZ_SERVICE_NAME, '/'),
+                                   boxee.core.DBUS_OM_IFACE)
+        objects = remote_om.GetManagedObjects()
+
+        for o, props in objects.iteritems():
+            if props.has_key(iface_name):
+                return o
+
+        err_msg = 'Adapter for interface [%s] not found. Exiting application...' % iface_name
+        print(err_msg)
+        logger.error(err_msg)
+        sys.exit(-1)
+
+    @staticmethod
+    def setup_logging(current_folder, log_level):
+        """
+        Configures the logging subsystem. By default everything goes to the syslog.
+        :param current_folder: the current program folder. where the debug log file shall be placed;
+        :param log_level: the desired log level to be set for the root logger (eg. logging.DEBUG)
+        :return:
+        """
+        logging.root.setLevel(log_level)
+
+        formatter = logging.Formatter('%(levelname)s - %(module)s.%(funcName)s: %(message)s')
+        syslog_handler = logging.handlers.SysLogHandler('/dev/log')
+        syslog_handler.setFormatter(formatter)
+        syslog_handler.setLevel(log_level)
+        logging.root.addHandler(syslog_handler)
+
+        # if log_level == 10:
+        #     logfile = current_folder + '/debug.log'
+        #     file_handler = logging.handlers.RotatingFileHandler(logfile)
+        #     file_handler.setFormatter(formatter)
+        #     file_handler.setLevel(log_level)
+        #     logging.root.addHandler(file_handler)
+        #     logger.info('Logging in: %s', logfile)
+
+        logger.debug('Log level enabled for DEBUG: [%s]' % logger.isEnabledFor(logging.DEBUG))
+        logger.debug('Log level enabled for INFO: [%s]' % logger.isEnabledFor(logging.INFO))
+        logger.debug('Log level enabled for ERROR: [%s]' % logger.isEnabledFor(logging.ERROR))
+        logger.debug('Log level enabled for WARNING: [%s]' % logger.isEnabledFor(logging.WARNING))
+        logger.debug('Syslog handler level: %s', syslog_handler.level)
+        logger.debug('Root logger level %s', logging.root.level)
+
+
+def usage():
+    print ('Usage:')
+    print ('\t -h --help \t list all command line options')
+    print ('\t -d --debug \t switches on the debug mode (more details in the syslog)')
+
+
+def main(argv):
+    boxee_server = None
+    log_level = logging.INFO
+    try:
+        current_folder = os.path.dirname(os.path.realpath(sys.argv[0]))
+        # more details on getopts: http://www.diveintopython.net/scripts_and_streams/command_line_arguments.html
+        opts, args = getopt.getopt(argv, "hd", ["help", "debug"])
+        for opt, arg in opts:
+            if opt in ("-h", "--help"):
+                usage()
+                sys.exit()
+            elif opt == '-d':
+                print ('\t :: activating debug mode')
+                log_level = logging.DEBUG
+        boxee_server = BoxeeServer(current_folder, log_level)
+        boxee_server.start_server()
+    except getopt.GetoptError:
+        usage()
+        sys.exit(2)
+    except BaseException as e:
+        print('Base exception received: {%s}' % str(e))
+    finally:
+        if boxee_server is not None:
+            boxee_server.stop_server()
+
+
+if __name__ == '__main__':
+    main(sys.argv[1:])  # chop off the sys.argv[0] which is the name of the script
