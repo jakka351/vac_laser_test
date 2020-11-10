2372bdc7618c540e0da0305c89d1ba9595a2ffba
jakka351 <57064943+jakka351@users.noreply.github.com>
2020-11-10 17:35:27 +1100

    gps pps install script finished
    Completed

--- install_gps.sh
+++ install_gps.sh
@@ -1,15 +1,45 @@
 #!/bin/bash
-#rpi_install.sh
-#this script will install vac_laser_test to a fresh copy of raspbian lite 
-############gpsd install & setup web app###########
-#
-sudo apt update -y && sudo apt dist-upgrade -y
-sudo apt install gpsd gpsd-clients python-gps python-serial python3-serial pps-tools ntp ntpstat -y
+  01001100 01100101 01101001 01100111 01101000 01110100 01101111 01101110
+  _          _       _     _              _____ _______      _
+ | |        (_)     | |   | |            |  _  ( ) ___ \    (_)
+ | |     ___ _  __ _| |__ | |_ ___  _ __ | | | |/| |_/ /_ __ _  ___ _ __
+ | |    / _ \ |/ _` | '_ \| __/ _ \| '_ \| | | | | ___ \ '__| |/ _ \ '_ \
+ | |___|  __/ | (_| | | | | || (_) | | | \ \_/ / | |_/ / |  | |  __/ | | |
+ \_____/\___|_|\__, |_| |_|\__\___/|_| |_|\___/  \____/|_|  |_|\___|_| |_|
+                __/ |
+               |___/
+  01001100 01100101 01101001 01100111 01101000 01110100 01101111 01101110
 
-sudo su 
+  Dri-Sump Containment Testing...                       ...Control Device
+                                          \  //\ /  `
+                                           \//~~\\__,
+                                          __  ___ __
+                                 |    /\ /__`|__ |__)
+                                 |___/~~\.__/|___|  \
+                                       ______ _____
+                                        ||__ /__`|
+                                        ||___.__/|
+
+
+  GPS + PPS Daemon Installer...
+                                                ....Debian-GNU/Linux 2020
+
+  01001100 01100101 01101001 01100111 01101000 01110100 01101111 01101110
+  Debian GNU/Linux comes with ABSOLUTELY NO WARRANTY, to the extent
+  permitted by applicable law.
+
+sudo su - <<EOF
+#sudo apt update -y && sudo apt dist-upgrade -y 
+sudo apt install -y gpsd gpsd-clients python-gps python-serial python3-serial pps-tools ntp ntpstat -y 
+echo "configuing gps services..." 
+echo ""
+
 #gpsd config file
 echo '
-# Default settings for the gpsd init script and the hotplug wrapper.
+# https://github.com/jakka351/vac_laser_test
+# Dri-Sump Containment Testing 
+# Leighton OBrien pty ltd
+# vaclasertest 08112020 /etc/default/gpsd 
 
 # Start the gpsd daemon automatically at boot time
 START_DAEMON="true"
@@ -23,10 +53,15 @@ DEVICES="/dev/ttyS0 /dev/pps0"
 
 # Other options you want to pass to gpsd
 GPSD_OPTIONS="-D 5 -N -n"
-' > /etc/default/gpsd
+' | sudo tee /etc/default/gpsd > /dev/null
 
 #services
-echo '[Unit]
+echo '
+# https://github.com/jakka351/vac_laser_test
+# Dri-Sump Containment Testing 
+# Leighton OBrien pty ltd
+# vaclasertest 08112020 /lib/systemd/system/gpsd.service
+[Unit]
 Description=GPS (Global Positioning System) Daemon
 Requires=gpsd.socket
 # Needed with chrony SOCK refclock
@@ -41,10 +76,10 @@ ExecStart=/usr/sbin/gpsd $GPSD_OPTIONS $DEVICES
 WantedBy=multi-user.target
 Also=gpsd.socket
 
-' > /lib/systemd/system/gpsd.service
+' | sudo tee /lib/systemd/system/gpsd.service > /dev/null
 
 echo '
-
+#vaclasertest 08112020
 [Unit]
 Description=GPS (Global Positioning System) Daemon Sockets
 
@@ -56,9 +91,13 @@ SocketMode=0600
 
 [Install]
 WantedBy=sockets.target
-' > /lib/systemd/system/gpsd.socket
+' | sudo tee /lib/systemd/system/gpsd.socket > /dev/null
 
 echo '
+# https://github.com/jakka351/vac_laser_test
+# Dri-Sump Containment Testing 
+# Leighton OBrien pty ltd
+# vaclasertest 08112020 /lib/systemd/system/gpsdctl@.service 
 [Unit]
 Description=Manage %I for GPS daemon
 Requires=gpsd.socket
@@ -74,11 +113,17 @@ RemainAfterExit=yes
 ExecStart=/bin/sh -c "[ \"$USBAUTO\" = true ] && /usr/sbin/gpsdctl add /dev/%I || :"
 ExecStop=/bin/sh -c "[ \"$USBAUTO\" = true ] && /usr/sbin/gpsdctl remove /dev/%I || :"
 
-' > /lib/systemd/system/gpsdctl@.service
-
-
+' | sudo tee /lib/systemd/system/gpsdctl@.service > /dev/null
+#
+echo "configuring pulse per second signal"
+#
 ###pps setup
 echo '
+# https://github.com/jakka351/vac_laser_test
+# Dri-Sump Containment Testing 
+# Leighton OBrien pty ltd
+# vaclasertest 08112020 /etc/ntp.conf
+
 # Kernel-mode PPS ref-clock for the precise seconds
 server 127.127.22.0 minpoll 4 maxpoll 4
 fudge 127.127.22.0 refid PPS stratum 0
@@ -86,74 +131,103 @@ fudge 127.127.22.0 refid PPS stratum 0
 # Server from shared memory provided by gpsd
 server 127.127.28.0 minpoll 4 maxpoll 4 prefer
 fudge 127.127.28.0 refid NMEA stratum 3 time 1 0.000
-' >> /etc/ntp.conf
-##check two arrows means add to file not replace
+' | sudo tee -a /etc/ntp.conf > /dev/null
+#
+echo "updating system time sync"
 echo '
+# https://github.com/jakka351/vac_laser_test
+# Dri-Sump Containment Testing 
+# Leighton OBrien pty ltd
+# vaclasertest 08112020 /etc/systemd/timesyncd.conf
+#pps time signal 
 [Time]
 NTP=127.127.28.0
-' >> /etc/systemd/timesyncd.conf
-
-#back to pi user
-su pi
-
-#gpsd service & socket
+' | sudo tee -a /etc/systemd/timesyncd.conf > /dev/null
+#
+#gpsd services
+#
+echo "starting gpsd service & socket..."
 sudo killall gpsd
-
+echo "sudo kill all gpsd"
 sudo systemctl enable gpsd.socket
 sudo systemctl start gpsd.socket
-
+echo "systemctl enable gpsd.socket"
+echo "systemctl start gpsd.socket"
 sudo systemctl enable gpsd.service
 sudo systemctl start gpsd.service
-
+echo "systemctl enable gpsd.service"
+echo "systemctl start gpsd.service"
 sudo systemctl enable gpsdctl@.service
 sudo systemctl start  gpsdctl@.service 
-
+echo "systemctl enable gpsdctl@.service"
+echo "systemctl start  gpsdctl@.service "
 sudo adduser gpsd pi dialout
 sudo gpsd -F /var/run/gpsd.sock 
+echo ""
 
+echo "creating gpsd directory..."
+#
 ###gps program
-sudo git clone ***gpslogger*** ./gpsd
-sudo mkdir /home/pi/logs
+#git clone ***gpslogger*** /home/pi/gpsd
+sudo mkdir /home/pi/gpsd 
+sudo mkdir /home/pi/gpsd/logs
 cd /home/pi/gpsd
-
-#root
-sudo su
+#
+#
+echo "creating pps.py script..."
 #python pps time script
 echo '                                                                   
+# https://github.com/jakka351/vac_laser_test
+# Dri-Sump Containment Testing 
+# Leighton OBrien pty ltd
+# vaclasertest 08112020 /home/pi/gpsd/pps.py 
+
+#pulse per second signal from ublox neo7 module pps pin = gpio 18
 import os
 import sys
 import time
 from gps import *
 
-print 'Attempting to access GPS time...'
+echo 'ocating GPS position.....'
 
 try:
         gpsd = gps(mode=WATCH_ENABLE)
 except:
-        print 'No GPS connection present. TIME NOT SET.'
+       
+       echo 'No GPS connection present. TIME NOT SET.'
         sys.exit()
 
 while True:
         gpsd.next()
         if gpsd.utc != None and gpsd.utc != '':
                 gpstime = gpsd.utc[0:4] + gpsd.utc[5:7] + gpsd.utc[8:10] + ' ' + gpsd.utc[11:19]
-                print 'Setting system time to GPS time...'
+               
+               echo '...setting system time to gps time...'
                 os.system('sudo date -u --set="%s"' % gpstime)
-                print 'System time set.'
+               
+               echo '....system time set'
+               
+               echo ''                
+               
+               echo 'Thanks uBlox'                
                 sys.exit()
         time.sleep(1)
 
 
-' > /home/pi/gpsd/pps.py
-
-su pi
+' | sudo tee /home/pi/gpsd/pps.py > /dev/null
+#
+#
 #make executable
+echo "making executable, setting to start on boot..."
 sudo chmod +x /home/pi/gpsd/pps.py
 #set system time
 sudo python /home/pi/gpsd/pps.py
+echo "..."
+echo ". . ."
 #set to start on boot
-sudo su
-echo 'sudo python /home/pi/gpsd/pps.py &' > /etc/rc.local
+#echo 'sudo python /home/pi/gpsd/pps.py &' | sudo tee -a /etc/rc.local > /dev/null
 #show date
-sudo date
-sudo reboot
+sudo date &&
+echo "system time"
+
+EOF
